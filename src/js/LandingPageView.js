66d9fec55d91dc47d64b7ece0bfcfab316950d96
Nathan Horowitz <nhorowitz@berkeley.edu>
2019-06-05 10:32:12 -0700

    sms

--- src/js/LandingPageView.js
+++ src/js/LandingPageView.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import "../css/LandingPageView.css"
 import firebase from "firebase"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import firebaseui from 'firebaseui'
 
 firebase.initializeApp({
     apiKey: "AIzaSyAtrvGB8pwYWmyRwG02svHpAkh5QWzh9yk",
@@ -16,16 +17,24 @@ firebase.initializeApp({
     //authDomain: "fir-auth-tutorial-ed11f.firebaseapp.com"
 })
 
-class LandingPageView extends Component {
-    state = { isSignedIn: false }
-    uiConfig = {
-        signInFlow: "popup",
-        signInOptions: [
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-            firebase.auth.GithubAuthProvider.PROVIDER_ID,
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+
+
+/**
+ * @return {!Object} The FirebaseUI config.
+ */
+function getUiConfig() {
+    return {
+        'callbacks': {
+            // Called when the user has been successfully signed in.
+            'signInSuccess': function(user, credential, redirectUrl) {
+                handleSignedInUser(user);
+                // Do not redirect.
+                return false;
+            }
+        },
+        // Opens IDP Providers sign-in flow in a popup.
+        'signInFlow': 'popup',
+        'signInOptions': [
             {
                 provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
                 recaptchaParameters: {
@@ -36,38 +45,114 @@ class LandingPageView extends Component {
                 }
             }
         ],
-        callbacks: {
-            signInSuccess: () => false //go to dashboardview
-        },
+        // Terms of service url.
         'tosUrl': 'https://www.google.com'
-    }
+    };
+}
 
-    componentDidMount = () => {
-        firebase.auth().onAuthStateChanged(user => {
-            this.setState({ isSignedIn: !!user })
-            console.log("user", user)
-        })
-    }
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+/**
+ * Displays the UI for a signed in user.
+ * @param {!firebase.User} user
+ */
+var handleSignedInUser = function(user) {
+    document.getElementById('user-signed-in').style.display = 'block';
+    document.getElementById('user-signed-out').style.display = 'none';
+    document.getElementById('name').textContent = user.displayName;
+    document.getElementById('email').textContent = user.email;
+    document.getElementById('phone').textContent = user.phoneNumber;
+    if (user.photoURL){
+        document.getElementById('photo').src = user.photoURL;
+        document.getElementById('photo').style.display = 'block';
+    } else {
+        document.getElementById('photo').style.display = 'none';
+    }
+};
+
+/**
+ * Displays the UI for a signed out user.
+ */
+var handleSignedOutUser = function() {
+    document.getElementById('user-signed-in').style.display = 'none';
+    document.getElementById('user-signed-out').style.display = 'block';
+    ui.start('#firebaseui-container', getUiConfig());
+};
+
+// Listen to change in auth state so it displays the correct UI for when
+// the user is signed in or not.
+firebase.auth().onAuthStateChanged(function(user) {
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('loaded').style.display = 'block';
+    user ? handleSignedInUser(user) : handleSignedOutUser();
+});
+
+/**
+ * Deletes the user's account.
+ */
+var deleteAccount = function() {
+    firebase.auth().currentUser.delete().catch(function(error) {
+        if (error.code == 'auth/requires-recent-login') {
+            // The user's credential is too old. She needs to sign in again.
+            firebase.auth().signOut().then(function() {
+                // The timeout allows the message to be displayed after the UI has
+                // changed to the signed out state.
+                setTimeout(function() {
+                    alert('Please sign in again to delete your account.');
+                }, 1);
+            });
+        }
+    });
+};
+
+/**
+ * Initializes the app.
+ */
+var initApp = function() {
+    document.getElementById('sign-out').addEventListener('click', function() {
+        firebase.auth().signOut();
+    });
+    document.getElementById('delete-account').addEventListener(
+        'click', function() {
+            deleteAccount();
+        });
+};
+
+window.addEventListener('load', initApp);
+
+
+class LandingPageView extends Component {
     render() {
         return (
-            <div className="LandingPageView">
-                {this.state.isSignedIn ? (
-                    <span>
-                        <div>Signed In!</div>
-                        <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
-                        <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
-                        <img
-                            alt="profile picture"
-                            src={firebase.auth().currentUser.photoURL}
-                        />
-                    </span>
-                ) : (
-                    <StyledFirebaseAuth
-                        uiConfig={this.uiConfig}
-                        firebaseAuth={firebase.auth()}
-                    />
-                )}
+            <div id="container">
+                <h3>TRAVELFAMPRIME</h3>
+                <div id="loading">Loading...</div>
+                <div id="loaded" class="hidden">
+                    <div id="main">
+                        <div id="user-signed-in" class="hidden">
+                            <div id="user-info">
+                                <div id="photo-container">
+                                    <img id="photo"></img>
+                                </div>
+                                <div id="name"></div>
+                                <div id="email"></div>
+                                <div id="phone"></div>
+                                <div class="clearfix"></div>
+                            </div>
+                            <p>
+                                <button id="sign-out">Sign Out</button>
+                                <button id="delete-account">Delete account</button>
+                            </p>
+                        </div>
+                        <div id="user-signed-out" class="hidden">
+                            <h4>You are signed out.</h4>
+                            <div id="firebaseui-spa">
+                                <div id="firebaseui-container"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
